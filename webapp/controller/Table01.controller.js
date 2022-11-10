76544eb8a9874f0df87123d58bb381d7e9556001
rndrps <filipp.sitdikov@gmail.com>
2022-11-10 18:07:56 +0300

    434

--- webapp/controller/Table01.controller.js
+++ webapp/controller/Table01.controller.js
@@ -44,12 +44,11 @@ sap.ui.define([
 				// var sData = oEvent.getSource().getModel("sOrder1").getData();
 				var oTable = this.byId("idSalesTable");
 				// selected row data containts selected records to edit
-				var selectedRowData = oTable;
+				var selectedRowData = oTable.getSelectedContexts();
 
-				// // oEvent.getSource().getParent().getParent().getParent().setShowFooter(true);
-				// that.getView().getModel("newModel").setProperty("/add", false);//we set property add to false so that it disappear when click on edit button
-				// that.getView().getModel("newModel").setProperty("/delete", false);//we set property delete to false so that it disappear when click on edit button
-				// that.getView().getModel("newModel").setProperty("/editable", false);//we set property editable to false so that first property is non-editable
+				console.log(oTable);
+				console.log(selectedRowData);
+				
 				selectedRowData.forEach(function (item) {
 					var sContext = item;
 					var sPath = sContext.getPath();
@@ -78,10 +77,6 @@ sap.ui.define([
 					that.getView().getModel("sOrder1").setProperty(sPath, sObj, sContext, true);
 
 				});
-				// // oEvent.getSource().getParent().getParent().setShowFooter(false);
-				// that.getView().getModel("newModel").setProperty("/edit", true);//we set property add to true so that it appers when click on save button
-				// that.getView().getModel("newModel").setProperty("/add", true);//we set property add to true so that it appers when click on save button
-				// that.getView().getModel("newModel").setProperty("/delete", true);//we set property delete to true so that it appers when click on save button
 
 				this.mode = "Null";
 
@@ -107,9 +102,6 @@ sap.ui.define([
 								sData[i].editable = false;//if record is not blank set editable to false
 								sData[i].neweditable = false;//this is for first property
 								that.getView().getModel("sOrder1").setProperty("/Sales/" + i, sData[i]);//set property binding for that records
-								// that.getView().getModel("newModel").setProperty("/edit", true);//edit button visible
-								// that.getView().getModel("newModel").setProperty("/add", true);//add button visible
-								// that.getView().getModel("newModel").setProperty("/delete", true);//delete button visible
 								that.getView().getModel("newModel").setProperty("/editable", false);//we set property editable to false so that first property is non-editable
 								// oEvent.getSource().getParent().getParent().setShowFooter(false);
 								MessageToast.show("Record saved Successfully");
@@ -128,10 +120,7 @@ sap.ui.define([
 		onAdd: function (oEvent) {
 			this.mode = "Add";
 			var that = this;
-			// oEvent.getSource().getParent().getParent().getParent().setShowFooter(true);
-			// that.getView().getModel("newModel").setProperty("/edit", false);
-			// that.getView().getModel("newModel").setProperty("/add", true);
-			// that.getView().getModel("newModel").setProperty("/delete", false);
+
 			that.getView().getModel("newModel").setProperty("/editable", true);
 
 			var newRecord = {//create a dummy record to push when user click on Add
@@ -199,102 +188,5 @@ sap.ui.define([
 				oTable.removeSelections(true);
 			}
 		}
-
-
-		// onSave: function(oEvent) {
-		// 	var that = this;
-		// 	if (this.mode === "Edit") {//if user click on save in edit functionality
-		// 		var oTable = this.byId("idSalesTable");
-		// 		var selectedRowData = oTable.getSelectedContexts();
-		// 		selectedRowData.forEach(function(item) {
-		// 			var sContext = item;
-		// 			var sPath = sContext.getPath();
-		// 			var sObj = sContext.getObject();
-		// 			sObj.editable = false;//we set editable false 
-		// 			that.getView().getModel("sOrder1").setProperty(sPath, sObj, sContext, true);
-
-		// 		});
-		// 		// oEvent.getSource().getParent().getParent().setShowFooter(false);
-		// 		that.getView().getModel("newModel").setProperty("/edit", true);//we set property add to true so that it appers when click on save button
-		// 		that.getView().getModel("newModel").setProperty("/add", true);//we set property add to true so that it appers when click on save button
-		// 		that.getView().getModel("newModel").setProperty("/delete", true);//we set property delete to true so that it appers when click on save button
-		// 		MessageToast.show("Record updated successfully");//throws a message
-		// 		return;
-
-		// 	} else if (this.mode === "Add") {
-		// 		var sData = oEvent.getSource().getModel("sOrder1").getData().Sales;//get the table data
-		// 		var sIndex = sData.length - 1;//get the length of the sdata
-		// 		if (this.recordexists !== undefined) {
-		// 			MessageToast.show("ID already exists");
-
-		// 			return;
-
-		// 		} else {
-		// 			for (var i = 0; i <= sIndex; i++) {
-
-		// 				if (sData[i].editable === true) {//check feilds which are Appended by click on Add
-		// 					if (sData[i].Id === "") {//check if the entered data is black then it throws a error message
-		// 						MessageToast.show("ID cannot be empty");
-		// 						return;
-		// 					} else {
-		// 						sData[i].editable = false;//if record is not blank set editable to false
-		// 						sData[i].neweditable = false;//this is for first property
-		// 						that.getView().getModel("sOrder1").setProperty("/Sales/" + i, sData[i]);//set property binding for that records
-		// 						that.getView().getModel("newModel").setProperty("/edit", true);//edit button visible
-		// 						that.getView().getModel("newModel").setProperty("/add", true);//add button visible
-		// 						that.getView().getModel("newModel").setProperty("/delete", true);//delete button visible
-		// 						that.getView().getModel("newModel").setProperty("/editable", false);//we set property editable to false so that first property is non-editable
-		// 						// oEvent.getSource().getParent().getParent().setShowFooter(false);
-		// 						MessageToast.show("Record saved Successfully");
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-
-		// }
-
-		// onCancel: function(oEvent) {
-		// 	if (this.mode === "Edit") {
-
-		// 		var that = this;
-		// 		var oTable = this.byId("idSalesTable");
-		// 		// var selectedRowData = oTable.getSelectedContexts();
-
-		// 		this.oldDataArry.forEach(function(item) {
-		// 			// var sContext = item;
-		// 			var sPath = item.sPath;
-		// 			var sObj = JSON.parse(item.sObj);
-		// 			sObj.editable = false;
-		// 			that.getView().getModel("sOrder1").setProperty(sPath, sObj, true);
-		// 		});
-		// 		// oEvent.getSource().getParent().getParent().setShowFooter(false);
-		// 		that.getView().getModel("newModel").setProperty("/edit", true);
-		// 		that.getView().getModel("newModel").setProperty("/add", true);
-		// 		that.getView().getModel("newModel").setProperty("/delete", true);
-		// 		oTable.removeSelections(true);
-
-		// 	} else if (this.mode === "Add") {
-		// 		 var oTable = this.byId("idSalesTable");
-		// 		var sData = oEvent.getSource().getModel("sOrder1").getData().Sales;
-		// 		var sIndex = sData.length - 1;
-		// 		for (var i = sIndex; i >= 0; i--) {
-		// 			var cellsInEdit = sData[i].editable;
-		// 			if (cellsInEdit === true) {
-		// 				sData.splice(i, 1);//delete a record by slice method
-
-		// 			}
-		// 			 oTable.removeSelections(true);
-		// 		}
-		// 		this.getView().getModel("sOrder1").setProperty("/Sales/", sData);
-		// 		// oEvent.getSource().getParent().getParent().setShowFooter(false);
-		// 		this.getView().getModel("newModel").setProperty("/edit", true);
-		// 		this.getView().getModel("newModel").setProperty("/add", true);
-		// 		this.getView().getModel("newModel").setProperty("/delete", true);
-		// 		MessageToast.show("Record saved Successfully");
-		// 	}
-
-		// }
-
 	});
 });
