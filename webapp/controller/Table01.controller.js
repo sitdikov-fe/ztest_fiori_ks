a5cea6989451480913a5c07c196596bf31602da5
rndrps <filipp.sitdikov@gmail.com>
2022-11-14 16:00:26 +0300

    3424

--- webapp/controller/Table01.controller.js
+++ webapp/controller/Table01.controller.js
@@ -30,100 +30,6 @@ sap.ui.define([
 			this.getView().setModel(newModel1, "newModel");
 		},
 
-		// onEdit: function (oEvent) {
-
-		// 	var that = this;
-
-		// 	if (this.mode === "Null") {//if user click on save in edit functionality
-
-		// 		// this.mode is a global varible 
-		// 		this.mode = "Edit";
-		// 		var that = this;
-		// 		// declare a arry for holding old records.
-		// 		this.oldDataArry = [];
-		// 		// var sData = oEvent.getSource().getModel("sOrder1").getData();
-		// 		var oTable = this.byId("idSalesTable");
-		// 		// selected row data containts selected records to edit
-		// 		var selectedRowData = oTable.getSelectedContexts();
-
-		// 		console.log(oTable);
-
-		// 		console.log(selectedRowData);
-
-		// 		if (selectedRowData.length === 0) {//this condiction check wheather the records got selected or not
-		// 			MessageToast.show("please select atleast one row");
-
-		// 			return;
-		// 		} else {
-
-		// 			that.getView().getModel("newModel").setProperty("/editable", false);//we set property editable to false so that first property is non-editable
-		// 			selectedRowData.forEach(function (item) {
-		// 				var sContext = item;
-		// 				var sPath = sContext.getPath();
-		// 				var sObj = sContext.getObject();
-		// 				var oldObj = {//here old Obj collects selected data 
-		// 					sPath: sPath,
-		// 					sObj: JSON.stringify(sObj)//Json.Stringfy method used to convert in String format
-		// 				};
-		// 				that.oldDataArry.push(oldObj);//append the record to arry which we declare before
-		// 				sObj.editable = true;//by using this property we enable inputfeilds visible
-
-		// 				that.getView().getModel("sOrder1").setProperty(sPath, sObj, sContext, true);//finally we set record in model in that path
-
-		// 			});
-
-		// 		}
-
-
-
-		// 	} else if (this.mode === "Edit") {//if user click on save in edit functionality
-		// 		var oTable = this.byId("idSalesTable");
-		// 		var selectedRowData = oTable.getSelectedContexts();
-		// 		selectedRowData.forEach(function (item) {
-		// 			var sContext = item;
-		// 			var sPath = sContext.getPath();
-		// 			var sObj = sContext.getObject();
-		// 			sObj.editable = false;//we set editable false 
-		// 			that.getView().getModel("sOrder1").setProperty(sPath, sObj, sContext, true);
-
-		// 		});
-
-		// 		this.mode = "Null";
-
-		// 		MessageToast.show("Record updated successfully");//throws a message
-		// 		return;
-
-		// 	} else if (this.mode === "Add") {
-		// 		var sData = oEvent.getSource().getModel("sOrder1").getData().Sales;//get the table data
-		// 		var sIndex = sData.length - 1;//get the length of the sdata
-		// 		if (this.recordexists !== undefined) {
-		// 			MessageToast.show("ID already exists");
-
-		// 			return;
-
-		// 		} else {
-		// 			for (var i = 0; i <= sIndex; i++) {
-
-		// 				if (sData[i].editable === true) {//check feilds which are Appended by click on Add
-		// 					if (sData[i].Id === "") {//check if the entered data is black then it throws a error message
-		// 						MessageToast.show("ID cannot be empty");
-		// 						return;
-		// 					} else {
-		// 						sData[i].editable = false;//if record is not blank set editable to false
-		// 						sData[i].neweditable = false;//this is for first property
-		// 						that.getView().getModel("sOrder1").setProperty("/Sales/" + i, sData[i]);//set property binding for that records
-		// 						that.getView().getModel("newModel").setProperty("/editable", false);//we set property editable to false so that first property is non-editable
-		// 						// oEvent.getSource().getParent().getParent().setShowFooter(false);
-		// 						MessageToast.show("Record saved Successfully");
-		// 						this.mode = "Null";
-		// 					}
-		// 				}
-		// 			}
-		// 		}
-		// 	}
-
-		// },
-
 		onLiveChange: function (oEvent) {
 			this.enteredValue = oEvent.getParameter("value");
 
@@ -165,7 +71,7 @@ sap.ui.define([
 			if (index > -1) {
 				this.recordexists = index;
 				that.getView().getModel("newModel").setProperty("/valueState", "Error");//set value state to error
-				MessageToast.show("Entered entry already exists");
+				MessageToast.show("Такая запись уже существует");
 
 				return;
 			}
