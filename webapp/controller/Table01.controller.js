e7a4caf557eb6066eaaedf9e57086974ad1d85b9
rndrps <filipp.sitdikov@gmail.com>
2022-12-12 20:03:14 +0300

    Update Table01.controller.js

--- webapp/controller/Table01.controller.js
+++ webapp/controller/Table01.controller.js
@@ -403,7 +403,7 @@ sap.ui.define([
 			var aTokens = oEvent.getParameter("tokens");
 			var str = aTokens[0].mProperties.key;
 			str = str.replace(/\s/g, '');
-			this._oMultiInput3.setValue(str);
+			// this._oMultiInput3.setValue(str);
 			// this._onChangeId(str);
 			this._oVHD3.close();
 		},
@@ -581,7 +581,7 @@ sap.ui.define([
 
 		onValueHelpOkPress4: function (oEvent) {
 			var aTokens = oEvent.getParameter("tokens");
-			this._oMultiInput4.setValue(aTokens[0].mProperties.key);
+			// this._oMultiInput4.setValue(aTokens[0].mProperties.key);
 			// this._onChangeId(aTokens[0].mProperties.key);
 			this._oVHD4.close();
 		},
