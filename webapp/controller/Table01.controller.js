ed23441bdf3130ca966ee5be6a646923e2dc15c9
rndrps <filipp.sitdikov@gmail.com>
2022-12-12 11:53:37 +0300

    Update Table01.controller.js

--- webapp/controller/Table01.controller.js
+++ webapp/controller/Table01.controller.js
@@ -276,6 +276,7 @@ sap.ui.define([
 								}
 							}
 						});
+
 						oTable3.addColumn(new UIColumn({
 							label: "Name",
 							template: "Name"
@@ -312,12 +313,12 @@ sap.ui.define([
 						});
 						oTable3.addColumn(new MColumn({
 							header: new Label({
-								text: "NameType"
+								text: "Name"
 							})
 						}));
 						oTable3.addColumn(new MColumn({
 							header: new Label({
-								text: "Name"
+								text: "NameType"
 							})
 						}));
 						oTable3.addColumn(new MColumn({
