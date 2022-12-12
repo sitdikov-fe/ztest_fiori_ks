4f388f8ef0610866b8fefa9ab83d0eaf09b800cb
rndrps <filipp.sitdikov@gmail.com>
2022-12-12 20:25:55 +0300

    321

--- webapp/controller/Table01.controller.js
+++ webapp/controller/Table01.controller.js
@@ -220,8 +220,11 @@ sap.ui.define([
 
 		// SH для позиции
 
-		onValueHelpRequested3: function () {
-			console.log(this._oMultiInput3.popa());
+		onValueHelpRequested3: function (oEvent) {
+			var sBindingContext = oEvent.getSource().getBindingContext();
+
+			var sPath = sBindingContext.sPath; 
+			console.log(sPath);
 			oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/");
 			this._oBasicSearchField3 = new SearchField();
 
