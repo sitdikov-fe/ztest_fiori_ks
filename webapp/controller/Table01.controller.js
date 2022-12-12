99c796ebf1904af242616bcf83ce7e1d63f4864f
rndrps <filipp.sitdikov@gmail.com>
2022-12-13 00:48:59 +0300

    Update Table01.controller.js

--- webapp/controller/Table01.controller.js
+++ webapp/controller/Table01.controller.js
@@ -617,6 +617,8 @@ sap.ui.define([
 
 		_onChangeStor: function (storage, nametype) {
 
+			oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZTEST_FIORI_KOSI_SRV/");
+
 			var mem = storage + nametype;
 
 			var readurl = "/ZtestshstorSet('" + mem + "')";
